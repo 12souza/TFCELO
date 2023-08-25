c64e10d5472322ccdfd0eef8d1d19d2ab4a3c3f5
bteitler <benteitler@gmail.com>
2023-08-24 19:22:10 -0700

    Ignore 0 scores as a temporary measure for false reports from other servers

--- NFOStats.py
+++ NFOStats.py
@@ -53,25 +53,31 @@ async def on_ready():
             begin = string.find(')') + 2
             end = string.find('\n') - 6
             region = string[begin:end]
-            reported_match = list(activePickups)[-1]
-            if (len(activePickups) > 0):
-                await pChannel.send(f"**AUTO-REPORTING** Reporting for game {reported_match}")
-                if ("Team 1 Wins" in (str(data))):
-                    #team 1 wins
-                    await pChannel.send("!win 1")
-                    #[MATCH RESULT] Team 1 Wins <10> (0)
-                    await pChannel.send(f'**AUTO-REPORTING** Team 1 wins {winningScore} to {losingScore}')
-                    await pChannel.send(f"!stats {region.lower()} {reported_match} {winningScore} {losingScore}")
-                elif ("Team 2 Wins" in (str(data))):
-                    await pChannel.send("!win 2")
-                    #[MATCH RESULT] Team 2 Wins <10> (0)
-                    await pChannel.send(f'**AUTO-REPORTING** Team 2 wins {winningScore} to {losingScore}')
-                    await pChannel.send(f"!stats {region.lower()} {reported_match} {winningScore} {losingScore}")
-                elif ("DRAW" in (str(data))):
-                    await pChannel.send("!draw")
-                    #[MATCH RESULT] DRAW at <0>
-                    await pChannel.send(f"**AUTO-REPORTING** DRAW at {losingScore}")
-                    await pChannel.send(f"!stats {region.lower()} {reported_match} {losingScore}")
+
+            if (losingScore == "0"):
+                print ("Ignoring match result due to 0")
+            else:
+                if (len(activePickups) > 0):
+                    reported_match = list(activePickups)[-1]
+                    await pChannel.send(f"**AUTO-REPORTING** Reporting for game {reported_match}")
+                    if ("Team 1 Wins" in (str(data))):
+                        #team 1 wins
+                        await pChannel.send("!win 1")
+                        #[MATCH RESULT] Team 1 Wins <10> (0)
+                        await pChannel.send(f'**AUTO-REPORTING** Team 1 wins {winningScore} to {losingScore}')
+                        await pChannel.send(f"!stats {region.lower()} {reported_match} {winningScore} {losingScore}")
+                    elif ("Team 2 Wins" in (str(data))):
+                        await pChannel.send("!win 2")
+                        #[MATCH RESULT] Team 2 Wins <10> (0)
+                        await pChannel.send(f'**AUTO-REPORTING** Team 2 wins {winningScore} to {losingScore}')
+                        await pChannel.send(f"!stats {region.lower()} {reported_match} {winningScore} {losingScore}")
+                    elif ("DRAW" in (str(data))):
+                        await pChannel.send("!draw")
+                        #[MATCH RESULT] DRAW at <0>
+                        await pChannel.send(f"**AUTO-REPORTING** DRAW at {losingScore}")
+                        await pChannel.send(f"!stats {region.lower()} {reported_match} {losingScore}")
+                else:
+                    print("!! NO ACTIVE PICKUPS!")
 
 client.run(v['TOKEN'])
 #client.run("NzMyMzcyMTcwMzY5NTMxOTc4.GPL0pm.iRN9voORDs1haOXvmlhZu26tWOtS-e7Xpmf7LM")
